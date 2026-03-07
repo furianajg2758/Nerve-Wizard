3d5d56d4fcdeeb3e94257f972d8975d78ebaf8e1
furiana <jg2758@gmail.com>
2026-03-07 14:06:47 -0800

    Changed root again

--- app/controllers/wizard_controller.rb
+++ app/controllers/wizard_controller.rb
@@ -7,9 +7,6 @@ class WizardController < ApplicationController
   before_action :set_steps
   before_action :setup_wizard
 
-  def start
-  end
-
   def restart
     session[:concern_id] = nil
     session[:diagnostic_id] = nil
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # root "posts#index"
 
 Rails.application.routes.draw do
-  root "wizard#choose_areas"
+  root "wizard#restart"
 
   resources :wizard, only: [:show, :update] do
     collection do
