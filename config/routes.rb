efbfc406a7505ff5460d7e95e05a7454c83fafef
furiana <jg2758@gmail.com>
2026-03-07 14:16:05 -0800

    Moved root to top of routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
-# config/routes.rb
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Point the root directly to the restart method
   root "wizard#restart"
+
+  get "up" => "rails/health#show", as: :rails_health_check
 
   resources :wizard, only: [:show, :update] do
     collection do
+      get :start
       get :restart
     end
   end
+
+  get "/restart", to: "wizard#restart"
 end
